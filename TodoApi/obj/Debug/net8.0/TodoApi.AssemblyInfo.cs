3d79a6a41bc5827a12a733428ddc7063f3858693
Nechami <h91064485@gmail.com>
2025-03-30 17:05:35 +0300

    sow strange

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa3d9be59bed144d74309a6adc13532356d1ddd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cce5ca8cf36d036988d38d8a4fe7a648b553486")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
